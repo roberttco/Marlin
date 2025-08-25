deb18ecdf2c117aa3a06944247dbb5d9f4b0800f
Robert Thompson <roberttco@gmail.com>
2025-08-24 21:19:53 -0600

    disable bezier and sd support

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2576,7 +2576,7 @@
 #endif
 
 // G5 Bézier Curve Support with XYZE destination and IJPQ offsets
-#define BEZIER_CURVE_SUPPORT        // Requires ~2666 bytes
+//#define BEZIER_CURVE_SUPPORT        // Requires ~2666 bytes
 
 #if ANY(ARC_SUPPORT, BEZIER_CURVE_SUPPORT)
   //#define CNC_WORKSPACE_PLANES      // Allow G2/G3/G5 to operate in XY, ZX, or YZ planes
